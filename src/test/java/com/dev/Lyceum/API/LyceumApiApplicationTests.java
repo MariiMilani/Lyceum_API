13b823014c10f8fc1c92689b9f5631ddaf9161ab
Mariana Milani <mari.m.assuncao@gmail.com>
2025-08-28 10:29:18 -0300

    first commit

---
+++ src/test/java/com/dev/Lyceum/API/LyceumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.Lyceum.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyceumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
